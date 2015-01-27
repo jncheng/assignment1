23844d7ac518c54082a1c252e5eebb4b7ff47be8
ai4281 <ai4281@gmail.com>
2015-01-27 10:31:20 -0800

    framework setup
    angular list making + data fething & vertical scrolling animation

---
+++ js/controllers.js
@@ -0,0 +1,47 @@
+//var myApp = angular.module('app', []);
+
+app.controller('MyController', function MyController($scope) {
+  $scope.answers = [
+  {
+    "id": "1",
+    "num":"1",
+    "detail":"Drop and roll"
+  },
+  {
+    "id": "1",
+    "num":"2",
+    "detail":"Hide under a table"
+  },
+  {
+    "id": "1",
+    "num":"3",
+    "detail":"Avoid Windows"
+  },
+  {
+    "id": "1",
+    "num":"4",
+    "detail":"Stay under powerlines"
+  },
+  {
+    "id": "2",
+    "num":"1",
+    "detail":"Drink water"
+  },
+  {
+    "id": "2",
+    "num":"2",
+    "detail":"Shake weight"
+  },
+  {
+    "id": "2",
+    "num":"3",
+    "detail":"Eat"
+  },
+  {
+    "id": "2",
+    "num":"4",
+    "detail":"Jump"
+  }
+]
+});
+
